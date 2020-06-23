49cbf231734191e152310e023c2b30895fa03bf8
Daniel Simango <daniel.simmz@gmail.com>
2020-06-23 11:51:38 -0500

    first commit

---
+++ eda-group-joins.sql
@@ -0,0 +1,24 @@
+--Tasks
+
+--1Get all customers and their addresses.
+SELECT 
+--2Get all orders and their line items (orders, quantity and product).
+
+--3Which warehouses have cheetos?
+
+--4Which warehouses have diet pepsi?
+
+--5Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+--6How many customers do we have?
+
+--7How many products do we carry?
+
+--8What is the total available on-hand quantity of diet pepsi?
+
+--Stretch
+--9How much was the total cost for each order?
+
+--10How much has each customer spent in total?
+
+--11How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
